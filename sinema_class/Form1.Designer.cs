e05a5d4a4b76187a9f59bc75eb45745e645ad830
İsmail İliksiz <ismail.iliksiz@mrevyap.k12.tr>
2021-12-27 20:27:40 +0300

    Bitirildi

--- sinema_class/Form1.Designer.cs
+++ sinema_class/Form1.Designer.cs
@@ -30,21 +30,24 @@ namespace sinema_class
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.groupBox5 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
             this.button5 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -54,6 +57,7 @@ namespace sinema_class
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Controls.Add(this.label1);
@@ -64,8 +68,35 @@ namespace sinema_class
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Salon Oluştur";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(92, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 71);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Oluştur";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(122, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(165, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Koltuk sayısı:";
+            // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.radioButton4);
             this.groupBox2.Controls.Add(this.radioButton1);
             this.groupBox2.Controls.Add(this.button2);
             this.groupBox2.Location = new System.Drawing.Point(358, 12);
@@ -75,8 +106,30 @@ namespace sinema_class
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Bilet Sat";
             // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(30, 42);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(76, 21);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "İndirimli";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(71, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 31);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Bilet Sat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // groupBox3
             // 
+            this.groupBox3.Controls.Add(this.radioButton3);
             this.groupBox3.Controls.Add(this.button3);
             this.groupBox3.Controls.Add(this.radioButton2);
             this.groupBox3.Location = new System.Drawing.Point(358, 173);
@@ -85,6 +138,27 @@ namespace sinema_class
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Bilet İptal";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(71, 84);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 31);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Bilet İptal";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(30, 41);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(76, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "İndirimli";
+            this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // groupBox4
             // 
@@ -97,6 +171,25 @@ namespace sinema_class
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Boş Koltuk Öğren";
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(71, 74);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 47);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Boş Koltuk Sorgula";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "__________";
+            // 
             // groupBox5
             // 
             this.groupBox5.Controls.Add(this.button5);
@@ -108,71 +201,15 @@ namespace sinema_class
             this.groupBox5.TabStop = false;
             this.groupBox5.Text = "Bakiye Öğren";
             // 
-            // label1
+            // button5
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 69);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(90, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Koltuk sayısı:";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(122, 69);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(165, 22);
-            this.textBox1.TabIndex = 1;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(91, 179);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(141, 71);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Oluştur";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(71, 82);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(90, 31);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "Bilet Sat";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(85, 46);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(76, 21);
-            this.radioButton1.TabIndex = 1;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "İndirimli";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(71, 43);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(76, 21);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "İndirimli";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(73, 46);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(88, 17);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "__________";
+            this.button5.Location = new System.Drawing.Point(71, 74);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(107, 50);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Bakiye Sorgula";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // label3
             // 
@@ -183,32 +220,36 @@ namespace sinema_class
             this.label3.TabIndex = 1;
             this.label3.Text = "__________";
             // 
-            // button3
+            // label4
             // 
-            this.button3.Location = new System.Drawing.Point(71, 84);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(90, 31);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Bilet İptal";
-            this.button3.UseVisualStyleBackColor = true;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 255);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(188, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Bilet 20, İndirimli Bilet 10tldir.";
             // 
-            // button4
+            // radioButton3
             // 
-            this.button4.Location = new System.Drawing.Point(71, 74);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(107, 47);
-            this.button4.TabIndex = 2;
-            this.button4.Text = "Boş Koltuk Sorgula";
-            this.button4.UseVisualStyleBackColor = true;
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(130, 42);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(87, 21);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "İndirimsiz";
+            this.radioButton3.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // radioButton4
             // 
-            this.button5.Location = new System.Drawing.Point(71, 74);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(107, 50);
-            this.button5.TabIndex = 2;
-            this.button5.Text = "Bakiye Sorgula";
-            this.button5.UseVisualStyleBackColor = true;
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(130, 42);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(87, 21);
+            this.radioButton4.TabIndex = 2;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "İndirimsiz";
+            this.radioButton4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -255,6 +296,9 @@ namespace sinema_class
         private System.Windows.Forms.GroupBox groupBox5;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
     }
 }
 
--- sinema_class/Form1.cs
+++ sinema_class/Form1.cs
@@ -16,10 +16,67 @@ namespace sinema_class
         {
             InitializeComponent();
         }
+        sinema s = new sinema();
 
         private void button1_Click(object sender, EventArgs e)
         {
+            s.makskoltuk = s.koltuk = int.Parse(textBox1.Text);
+            s.kasa = 0;
+            
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (s.koltuk!=0)
+            {
+                if (radioButton1.Checked)
+                {
+                    s.kasa = s.kasa + 10;
+                }
+                else
+                {
+                    s.kasa = s.kasa + 20;
+                }
+                s.koltuk = s.koltuk - 1;
+            }
+            else
+            {
+                MessageBox.Show("Hata! Bütün Koltuklar Doludur, Bilet Satılamaz");
+            }
+
+
+            
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (s.koltuk < s.makskoltuk)
+            {
+                if (radioButton2.Checked)
+                {
+                    s.kasa = s.kasa - 10;
+                }
+                else
+                {
+                    s.kasa = s.kasa - 20;
+                }
+                s.koltuk = s.koltuk + 1;
+            }
+            else
+            {
+                MessageBox.Show("Hata! Bütün Koltuklar Doludur, Bilet Satılamaz");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label2.Text = s.koltuk.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label3.Text = s.kasa.ToString();
         }
     }
 }
